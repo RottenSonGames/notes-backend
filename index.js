8a2faf34fbedd6c08efb48252b68a3fdfe04fd11
RottenSonGames <sanghoyou@gmail.com>
2025-04-14 22:02:47 +0900

    DB and backend connection

--- index.js
+++ index.js
@@ -26,15 +26,15 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => response.json(note))
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+      .then(note => {
+        if (note) response.json(note)
+        else response.status(404).end()
+      })
+      .catch(error => next(error))
+      
 })
-
-const generateId = () => {
-  const maxId =
-    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0
-  return String(maxId + 1)
-}
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
@@ -50,11 +50,31 @@ app.post('/api/notes', (request, response) => {
   note.save().then(savedNote => response.json(savedNote))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter((note) => note.id !== id)
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
 
-  response.status(204).end()
+  Note.findById(request.params.id)
+    .then(note => {
+      if (!note) {
+        return response.status(404).end()
+      }
+
+      note.content = content
+      note.important = important
+
+      return note.save().then((updatedNote) => {
+        response.json(updatedNote)
+      })
+    })
+    .catch(error => next(error))
+})
+
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (request, response) => {
@@ -63,7 +83,24 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+app.use(errorHandler)
+
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
+// const generateId = () => {
+//   const maxId =
+//     notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0
+//   return String(maxId + 1)
+// }
