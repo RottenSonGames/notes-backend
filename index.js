1161a7790969c3e0c46101dc82b6b5799531c3fa
RottenSonGames <sanghoyou@gmail.com>
2025-04-17 21:04:20 +0900

    eslint

--- index.js
+++ index.js
@@ -28,19 +28,19 @@ app.get('/api/notes', (request, response) => {
 
 app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
-      .then(note => {
-        if (note) response.json(note)
-        else response.status(404).end()
-      })
-      .catch(error => next(error))
-      
+    .then(note => {
+      if (note) response.json(note)
+      else response.status(404).end()
+    })
+    .catch(error => next(error))
+
 })
 
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
   if (!body.content) {
-    return response.status(400).json({error: 'content missing',})
+    return response.status(400).json({ error: 'content missing', })
   }
 
   const note = new Note({
@@ -88,7 +88,7 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  }
 
   next(error)
 }
--- mongo.js
+++ mongo.js
@@ -4,32 +4,32 @@ if (process.argv.length < 3) {
   console.log('give password as argument')
   process.exit(1)
 }
-  
+
 const password = process.argv[2]
 const url = `mongodb://sanghoyou:${password}@ac-2parfjc-shard-00-00.optkffg.mongodb.net:27017,ac-2parfjc-shard-00-01.optkffg.mongodb.net:27017,ac-2parfjc-shard-00-02.optkffg.mongodb.net:27017/noteApp?replicaSet=atlas-vml457-shard-0&ssl=true&authSource=admin&retryWrites=true&w=majority&appName=Clusterbator`
 
 mongoose.set('strictQuery',false)
 mongoose.connect(url)
-  
+
 const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
 })
-  
+
 const Note = mongoose.model('Note', noteSchema)
 
-  // Note.find({}).then(result => {
-  //   result.forEach(note => {
-  //       console.log(note)
-  //   })
-  //   mongoose.connection.close()
-  // })
-  
+// Note.find({}).then(result => {
+//   result.forEach(note => {
+//       console.log(note)
+//   })
+//   mongoose.connection.close()
+// })
+
 const note1 = new Note({
   content: 'HTML is easy',
   important: true,
 })
-  
+
 note1.save().then(result => {
   console.log('note saved!')
 })
@@ -38,7 +38,7 @@ const note2 = new Note({
   content: 'Mongoose makes use of mongo easy',
   important: true,
 })
-  
+
 note2.save().then(result => {
   console.log('note saved!')
 })
@@ -47,7 +47,7 @@ const note3 = new Note({
   content: 'Callback functions are sucky',
   important: true,
 })
-  
+
 note3.save().then(result => {
   console.log('note saved!')
   mongoose.connection.close()
