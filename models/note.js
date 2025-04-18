80a4dd7d9fc32189db31d6d1b4e5b0e71df3c656
RottenSonGames <sanghoyou@gmail.com>
2025-04-18 17:52:31 +0800

    restructured backend

--- models/note.js
+++ models/note.js
@@ -8,7 +8,11 @@ mongoose.connect(url)
   .catch(error => console.log('error connecting to MongoDB:', error.message))
 
 const noteSchema = new mongoose.Schema({
-  content: String,
+  content: {
+    type: String,
+    minLength: 5,
+    required: true
+  },
   important: Boolean,
 })
 
