b974a7f68051b7ceccec30b2874a9614a8bea3f9
RottenSonGames <sanghoyou@gmail.com>
2025-07-23 16:52:20 +0800

    some token stuff

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,6 +1,18 @@
+const jwt = require('jsonwebtoken')
+
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 const User = require('../models/user')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+
+  return null
+}
 
 notesRouter.get('/', async (request, response) => {
   const notes = await Note.find({})
@@ -19,6 +31,12 @@ notesRouter.get('/:id', async (request, response, next) => {
 
 notesRouter.post('/', async (request, response, next) => {
   const body = request.body
+
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'token invalid '})
+  }
 
   const user = await User.findById(body.userId)
 
